35efad2a3477377daa72f46b7ff773cf262de65a
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-13 11:30:42 +0900

    add login test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { User } from '../model/Model';
+import { AuthService } from '../services/AuthService';
+import { Login } from './Login';
 
-function App() {
-  return <div>App works</div>;
+interface AppState {
+  user: User | undefined;
 }
 
-export default App;
+export class App extends React.Component<{}, AppState> {
+  private authService: AuthService = new AuthService();
+
+  constructor(props: any) {
+    super(props);
+
+    this.setUser = this.setUser.bind(this);
+  }
+
+  private setUser(user: User) {
+    this.setState({
+      user: user,
+    });
+    console.log('setting the user!:' + user);
+  }
+
+  render() {
+    return (
+      <div>
+        App from class works!
+        <Login authService={this.authService} setUser={this.setUser} />
+      </div>
+    );
+  }
+}
